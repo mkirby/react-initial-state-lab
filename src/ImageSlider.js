ba9bc504ee86b259d6c2b4faa552a737f176e6d3
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-16 18:31:34 -0500

    finished lab

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,15 @@
-// your ImageSlider code here!
+import { render } from 'enzyme'
+import React from 'react'
+
+export default class ImageSlider extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return <div>I am on slide {this.state.currentSlideIndex}</div>
+  }
+}
